3957addcc350c344539fe7aa7255925d7f34f9b6
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-09-17 23:40:52 +0100

    Added Controllers for Book, Author, used Thymeleaf templating

--- spring5webapp/src/main/java/com/spring5/projects/spring5webapp/domain/Author.java
+++ spring5webapp/src/main/java/com/spring5/projects/spring5webapp/domain/Author.java
@@ -60,7 +60,7 @@ public class Author {
     @Override
     public String toString() {
         return "Author{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" +
-               lastName + '\'' + ", books=" + books + '}';
+               lastName + '\'' + '}';
     }
 
     @Override
