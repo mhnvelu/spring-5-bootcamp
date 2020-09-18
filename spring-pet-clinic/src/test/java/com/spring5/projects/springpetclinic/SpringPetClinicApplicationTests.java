8cb1140aa31178fb1819a0d0887c056b6b8847fb
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-09-18 23:03:35 +0100

    Spring Pet Clinic Project

---
+++ spring-pet-clinic/src/test/java/com/spring5/projects/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.projects.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
