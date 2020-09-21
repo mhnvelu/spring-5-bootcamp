1e4135eba1e9ca1ea5369167dee56c42fa7bacfd
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-09-21 11:50:10 +0100

    Spring DI - ApplicationContext

---
+++ spring-di/src/main/java/com/spring5/projects/springdi/controllers/MyController.java
@@ -0,0 +1,11 @@
+package com.spring5.projects.springdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        return "Hello Folks!!!";
+    }
+}
---
+++ spring-di/src/test/java/com/spring5/projects/springdi/SpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.projects.springdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
