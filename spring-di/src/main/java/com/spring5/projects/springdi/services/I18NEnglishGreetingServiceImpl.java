9d0b7b71b2e838be1919bae0a84c0284c47082c6
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-09-26 23:48:51 +0100

    Spring Bean Factory

--- spring-di/src/main/java/com/spring5/projects/springdi/services/I18NEnglishGreetingServiceImpl.java
+++ spring-di/src/main/java/com/spring5/projects/springdi/services/I18NEnglishGreetingServiceImpl.java
@@ -3,8 +3,7 @@ package com.spring5.projects.springdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
-@Service("i18nService")
+//@Service("i18nService")
 public class I18NEnglishGreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
--- spring-di/src/main/java/com/spring5/projects/springdi/services/I18NSpanishGreetingServiceImpl.java
+++ spring-di/src/main/java/com/spring5/projects/springdi/services/I18NSpanishGreetingServiceImpl.java
@@ -3,8 +3,7 @@ package com.spring5.projects.springdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile({"ES","default"})
-@Service("i18nService")
+//@Service("i18nService")
 public class I18NSpanishGreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
