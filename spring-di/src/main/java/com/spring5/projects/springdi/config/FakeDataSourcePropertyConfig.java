05b47340391b98f0dd6c98768c38697daebbd3b0
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-09-29 00:00:24 +0100

    Load Environment variables

--- spring-di/src/main/java/com/spring5/projects/springdi/config/FakeDataSourcePropertyConfig.java
+++ spring-di/src/main/java/com/spring5/projects/springdi/config/FakeDataSourcePropertyConfig.java
@@ -1,15 +1,20 @@
 package com.spring5.projects.springdi.config;
 
 import com.spring5.projects.springdi.datasource.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class FakeDataSourcePropertyConfig {
+
+    @Autowired
+    Environment environment;
 
     @Value("${project.db.username}")
     private String username;
@@ -21,19 +26,18 @@ public class FakeDataSourcePropertyConfig {
     private String dbUrl;
 
     @Bean
-    public FakeDataSource fakeDataSource() {
-        FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(username);
-        fakeDataSource.setPassword(password);
-        fakeDataSource.setDbUrl(dbUrl);
-        return fakeDataSource;
-    }
-
-
-    @Bean
     public static PropertySourcesPlaceholderConfigurer properties() {
         PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
                 new PropertySourcesPlaceholderConfigurer();
         return propertySourcesPlaceholderConfigurer;
+    }
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(environment.getProperty("DB_USERNAME"));
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setDbUrl(dbUrl);
+        return fakeDataSource;
     }
 }
