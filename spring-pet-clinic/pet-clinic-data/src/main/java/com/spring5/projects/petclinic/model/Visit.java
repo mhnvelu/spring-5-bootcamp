ee54d7421a2631196221f64f396ac3e70c5b3d34
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-09-30 18:53:17 +0100

    Create PetType, Pet, Visit entities. Closes #26.

---
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/spring5/projects/petclinic/model/Visit.java
@@ -0,0 +1,34 @@
+package com.spring5.projects.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+
+    private LocalDate visitDate;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(LocalDate visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
