1b2f084ba13ef4b057fff32d510d36efb2f612f4
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-10-01 18:18:29 +0100

    Spring Receipe Project

---
+++ spring-recipe-project/src/main/java/com/spring5/projects/springrecipeproject/SpringRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.projects.springrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeProjectApplication.class, args);
+    }
+
+}
---
+++ spring-recipe-project/src/test/java/com/spring5/projects/springrecipeproject/SpringRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.projects.springrecipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
