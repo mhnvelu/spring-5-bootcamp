0468ae766fb018f4a0656dc1b9f40314e8c31bbc
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-10-05 17:04:54 +0100

    Add Project Lombok and refactor. Closes #59.

--- spring-pet-clinic/pet-clinic-data/src/main/java/com/spring5/projects/petclinic/model/Visit.java
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/spring5/projects/petclinic/model/Visit.java
@@ -1,8 +1,15 @@
 package com.spring5.projects.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
@@ -16,28 +23,4 @@ public class Visit extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "pet_id")
     private Pet pet;
-
-    public LocalDate getVisitDate() {
-        return visitDate;
-    }
-
-    public void setVisitDate(LocalDate visitDate) {
-        this.visitDate = visitDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
