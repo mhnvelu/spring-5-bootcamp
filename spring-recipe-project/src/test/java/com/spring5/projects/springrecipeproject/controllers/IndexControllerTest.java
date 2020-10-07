6d47d460784049fb8fc749d58a1fa1e6d9b5b89d
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-10-07 18:11:03 +0100

    Spring Recipe Project - JUnit test cases - Mockito Argument capture

--- spring-recipe-project/src/test/java/com/spring5/projects/springrecipeproject/controllers/IndexControllerTest.java
+++ spring-recipe-project/src/test/java/com/spring5/projects/springrecipeproject/controllers/IndexControllerTest.java
@@ -1,17 +1,20 @@
 package com.spring5.projects.springrecipeproject.controllers;
 
+import com.spring5.projects.springrecipeproject.domain.Recipe;
 import com.spring5.projects.springrecipeproject.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -31,9 +34,24 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(2L);
+
+        recipes.add(recipe);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
         String page = indexController.getIndexPage(model);
         assertEquals("index", page);
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
         verify(recipeService, times(1)).getRecipes();
+
+        Set<Recipe> recipeSet = argumentCaptor.getValue();
+        assertEquals(2,recipeSet.size());
     }
 }
