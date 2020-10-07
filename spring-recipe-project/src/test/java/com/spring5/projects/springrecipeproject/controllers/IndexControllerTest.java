0f39c3d67107b6a28d3f3cb165d5f511368c270a
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-10-07 18:54:56 +0100

    Spring Recipe Project - JUnit test cases - Spring MockMVC

--- spring-recipe-project/src/test/java/com/spring5/projects/springrecipeproject/controllers/IndexControllerTest.java
+++ spring-recipe-project/src/test/java/com/spring5/projects/springrecipeproject/controllers/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -15,6 +17,9 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -42,6 +47,7 @@ class IndexControllerTest {
 
         recipes.add(recipe);
 
+        // Mockito Argument captor
         ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
         when(recipeService.getRecipes()).thenReturn(recipes);
@@ -52,6 +58,13 @@ class IndexControllerTest {
         verify(recipeService, times(1)).getRecipes();
 
         Set<Recipe> recipeSet = argumentCaptor.getValue();
-        assertEquals(2,recipeSet.size());
+        assertEquals(2, recipeSet.size());
+    }
+
+    @Test
+    void testMockMVC() throws Exception {
+        // Spring MockMVC
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
     }
 }
