2fd807e43a03cfa5a9664f0e44150f0718485b35
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-10-31 00:21:48 +0000

    Spring Recipe Project - @ControllerAdvice annotation

--- spring-recipe-project/src/test/java/com/spring5/projects/springrecipeproject/controllers/ImageControllerTest.java
+++ spring-recipe-project/src/test/java/com/spring5/projects/springrecipeproject/controllers/ImageControllerTest.java
@@ -33,7 +33,8 @@ class ImageControllerTest {
     void setUp() {
         MockitoAnnotations.initMocks(this);
         imageController = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController)
+                .setControllerAdvice(new ControllerExceptionHandler()).build();
     }
 
     @Test
@@ -91,5 +92,11 @@ class ImageControllerTest {
                         .getResponse();
         byte[] content = response.getContentAsByteArray();
         assertEquals(img.getBytes().length, content.length);
+    }
+
+    @Test
+    void getImageNumberFormatException() throws Exception {
+        mockMvc.perform(get("/recipe/one/imageupload")).andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 }
--- spring-recipe-project/src/test/java/com/spring5/projects/springrecipeproject/controllers/RecipeControllerTest.java
+++ spring-recipe-project/src/test/java/com/spring5/projects/springrecipeproject/controllers/RecipeControllerTest.java
@@ -32,7 +32,8 @@ public class RecipeControllerTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         recipeController = new RecipeController(recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(recipeController).
+                setControllerAdvice(new ControllerExceptionHandler()).build();
     }
 
     @Test
