e54f2aff05dc3faff0c2b9ce942782ed2a243fe7
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-01-08 14:00:03 +0000

    Spring5-rest-services project - using UriComponentsBuilder

--- spring5-rest-services/src/main/java/com/spring5/rest/app/services/UserApiServiceImpl.java
+++ spring5-rest-services/src/main/java/com/spring5/rest/app/services/UserApiServiceImpl.java
@@ -2,25 +2,33 @@ package com.spring5.rest.app.services;
 
 import com.spring5.rest.app.domain.User;
 import com.spring5.rest.app.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @Service
 public class UserApiServiceImpl implements UserApiService {
 
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
-    public UserApiServiceImpl(RestTemplate restTemplate) {
+    private String apiUrl;
+
+    public UserApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiUrl) {
         this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
     }
 
     @Override
     public List<User> getUserData(int limit) {
-        UserData userData = restTemplate.getForObject(
-                "https://private-anon-75abfa99b2" + "-apifaketory" + ".apiary-mock" +
-                ".com/api/user", UserData.class);
+
+        UriComponentsBuilder uriComponentsBuilder =
+                UriComponentsBuilder.fromUriString(apiUrl).queryParam("limit", limit);
+
+        UserData userData =
+                restTemplate.getForObject(uriComponentsBuilder.toUriString(), UserData.class);
         return userData.getData();
     }
 }
