4242b3571fe19470f655b7ae82a8d50ae5cdcb6b
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-01-08 17:38:00 +0000

    Spring5-rest-services project - using Reactive WebClient

--- spring5-rest-services/src/main/java/com/spring5/rest/app/services/UserApiService.java
+++ spring5-rest-services/src/main/java/com/spring5/rest/app/services/UserApiService.java
@@ -1,10 +1,13 @@
 package com.spring5.rest.app.services;
 
 import com.spring5.rest.app.domain.User;
-import com.spring5.rest.app.domain.UserData;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface UserApiService {
     List<User> getUserData(int limit);
+
+    Flux<User> getUsers(Mono<Integer> limit);
 }
